b26b45f1ae9a7dc64c39168de70897f72e671f6c
guozhongxiang <guozhongxiang@DESKTOP-HQLML1F>
2019-12-25 17:14:41 +0800

    初始化架构

---
+++ src/gTimedTask.Core/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gTimedTask.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        public string Index() {
+            return "hello";
+        }
+    }
+}
