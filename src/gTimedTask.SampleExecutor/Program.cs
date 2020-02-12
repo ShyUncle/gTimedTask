ac46311b1ae0c86df6ba97048079a8a165ff5e94
guozhongxiang <shyuncle_gzx@163.com>
2020-02-12 21:22:17 +0800

    test

--- src/gTimedTask.SampleExecutor/Program.cs
+++ src/gTimedTask.SampleExecutor/Program.cs
@@ -20,7 +20,7 @@ namespace gTimedTask.Executor
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                { 
                     webBuilder.UseStartup<Startup>();
                 });
     }
